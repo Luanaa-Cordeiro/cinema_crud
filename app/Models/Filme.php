6e7fd923cdb8c1bb4c4d419291a1b643145bfb5c
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2024-12-08 15:24:40 -0300

    models da chave estrangeira

--- app/Models/Filme.php
+++ app/Models/Filme.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Filme extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'classificacao',
+        'ano',
+        'id_genero',
+    ];
+
+    protected $table = 'filmes';
+
+    public function genero()
+    {
+        return $this->belongsTo(Genero::class, 'id_genero', 'id');
+    }
 }
--- app/Models/Genero.php
+++ app/Models/Genero.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Genero extends Model
 {
+    use HasFactory;
 
     protected $fillable = [
         'nome',
     ];
+
+    protected $table = 'generos';
+
+    public function filmes()
+    {
+        return $this->hasMany(Filme::class, 'id_genero', 'id');
+    }
 }
