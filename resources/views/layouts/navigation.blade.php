da348431461296cf2a2a54bb480ef88e989a282a
luanaa-cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2025-04-15 21:32:40 -0300

    removendo salas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,11 +35,7 @@
                     </x-nav-link>
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('salas.index')" :active="request()->routeIs('salas')" style="color:white;">
-                        {{ __('Salas')}}
-                    </x-nav-link>
-                </div>
+               
             </div>
 
             <!-- Settings Dropdown -->
